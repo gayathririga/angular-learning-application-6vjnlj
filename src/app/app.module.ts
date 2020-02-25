9a6eed7de69dae9d92992b06d2119bb6171b35c4
gayathririga <mailtogayathri05@gmail.com>
2020-02-25 11:45:37 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -16,7 +17,8 @@ import { ServersComponent } from './servers/servers.component';
   imports:      [ 
     BrowserModule,
     FormsModule,
-    HttpModule 
+    HttpModule,
+    NgbModule 
   ],  
   providers:    [],
   bootstrap:    [ AppComponent ]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created.";
+  serverName = "Two way binding works";
 
   constructor() {
     setTimeout(()=> {
@@ -17,6 +18,10 @@ export class ServersComponent implements OnInit {
   
   onCreateServer(){
     this.serverCreationStatus = "Server is created";
+  }
+
+  onUpdateServer(event: any){
+    this.serverName = event.target.value;
   }
 
   ngOnInit() {
